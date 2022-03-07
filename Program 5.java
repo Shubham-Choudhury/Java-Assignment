6105537e45b5f0560d5323b01d7feb2e5314d57c
Shubham Choudhury <69568389+Shubham-Choudhury@users.noreply.github.com>
2022-03-07 23:49:17 +0530

    Create Program 5.java

---
+++ Program 5.java
@@ -0,0 +1,32 @@
+// Write a Java Program to Check Whether a Number is Perfect Number or Not
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int oriNum = sc.nextInt();
+        int num = oriNum;
+        int rem, newNum = 0;
+        while(num != 0){
+            rem = num % 10;
+            newNum += (rem * rem * rem);
+            num /= 10;
+        }
+        if(oriNum == newNum){
+            System.out.println( oriNum + " is a Armstrong Number");
+        }
+        else{
+            System.out.println( oriNum + " is not a Armstrong Number");
+        }
+        sc.close();
+    }
+}
+
+/* 
+        OUTPUT
+    Enter a Number: 
+    407
+    407 is a Armstrong Number
+*/
