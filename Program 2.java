db5c4c6b684f59ca9146b25f91ed02f18ad34a90
Shubham Choudhury <69568389+Shubham-Choudhury@users.noreply.github.com>
2022-03-07 23:07:16 +0530

    Update Program 2.java

--- Program 2.java
+++ Program 2.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class problem4 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number: ");
