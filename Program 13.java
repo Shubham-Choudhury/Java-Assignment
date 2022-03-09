ef23d8af98bf2491fe57a2e03941a5136b7ee3a4
Shubham Choudhury <69568389+Shubham-Choudhury@users.noreply.github.com>
2022-03-09 08:59:18 +0530

    Create Program 13.java

---
+++ Program 13.java
@@ -0,0 +1,58 @@
+// Write a Java program to count the occurrences of each character
+
+import java.util.Scanner;
+
+class Main {
+    static final int MAX_CHAR = 256;
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = sc.nextLine();
+        getOccuringChar(str);
+        sc.close();
+    }
+
+    static void getOccuringChar(String str)
+    {
+        int count[] = new int[MAX_CHAR];
+        int len = str.length();
+        for (int i = 0; i < len; i++){
+            count[str.charAt(i)]++;
+        }
+        char ch[] = new char[str.length()];
+        for (int i = 0; i < len; i++) {
+            ch[i] = str.charAt(i);
+            int find = 0;
+            for (int j = 0; j <= i; j++) {
+                if (str.charAt(i) == ch[j])
+                    find++;
+            }
+ 
+            if (find == 1)
+                System.out.println("Number of Occurrence of " + str.charAt(i) + " is:" + count[str.charAt(i)]);
+        }
+    }
+}
+/*
+        OUTPUT
+    Enter a String: 
+    Triveni Devi Bhalotia College
+    Number of Occurrence of T is:1
+    Number of Occurrence of r is:1
+    Number of Occurrence of i is:4
+    Number of Occurrence of v is:2
+    Number of Occurrence of e is:4
+    Number of Occurrence of n is:1
+    Number of Occurrence of   is:3
+    Number of Occurrence of D is:1
+    Number of Occurrence of B is:1
+    Number of Occurrence of h is:1
+    Number of Occurrence of a is:2
+    Number of Occurrence of l is:3
+    Number of Occurrence of o is:2
+    Number of Occurrence of t is:1
+    Number of Occurrence of C is:1
+    Number of Occurrence of g is:1
+*/
