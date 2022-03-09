50d8aa3fc6748e09b3253a555a9db62970201d8d
Shubham Choudhury <69568389+Shubham-Choudhury@users.noreply.github.com>
2022-03-09 09:04:24 +0530

    Update Program 11.java

--- Program 11.java
+++ Program 11.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class problem13 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of array elements: ");
